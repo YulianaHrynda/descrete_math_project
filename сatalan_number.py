3d58332b9197c15b24cf9acd00eed4bb5386ca24
YulianaHrynda <146963885+YulianaHrynda@users.noreply.github.com>
2023-11-29 14:31:16 +0200

    Update сatalan_number.py
    added functions

--- сatalan_number.py
+++ сatalan_number.py
@@ -1,1 +1,14 @@
+def integer_paths():
+    pass
 
+def diagonals():
+    pass
+
+def parenthesis_sequences():
+    pass
+
+def catalan_numbers():
+    pass
+
+def catalan_number_formula():
+    pass
