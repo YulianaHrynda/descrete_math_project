fd4ef50dfe0201710f2a27ba1f46604c208ec523
Yuliana Hrynda <hrynda.pn@ucu.edu.ua>
2023-11-30 22:08:24 +0200

    created function that finds n Catalans numbers recursively

--- сatalan_number.py
+++ сatalan_number.py
@@ -7,8 +7,24 @@ def diagonals():
 def parenthesis_sequences():
     pass
 
-def catalan_numbers():
-    pass
+def catalan_numbers(n: int, m: int = 1, nums: list = [1, 1]):
+    """
+    The function that finds the first n Catalan numbers using a recursive formula
+    >>> catalan_numbers(10)
+    [1, 1, 2, 5, 14, 42, 132, 429, 1430, 4862, 16796]
+    """
+    if n == 0 or n == 1:
+        return 1
+
+    if m != n:
+        catalan_num = 0
+        for k in range(m + 1):
+            catalan_num += nums[k] * nums[m - k]
+        nums.append(catalan_num)
+        m += 1
+        return catalan_numbers(n, m, nums)
+    else:
+        return nums
 
 def catalan_number_formula():
     pass
