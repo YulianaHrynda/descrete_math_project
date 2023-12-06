1edd3bbec067fcd0a858e3299cf28ee0fc86a88e
YulianaHrynda <146963885+YulianaHrynda@users.noreply.github.com>
2023-12-06 13:45:34 +0200

    Update сatalan_number.py

--- сatalan_number.py
+++ сatalan_number.py
@@ -18,7 +18,7 @@ def catalan_numbers(n: int, m: int = 1, nums: list = [1, 1]):
     elif n == 1:
         return [1, 1]
 
-    if m != n:
+    if m != n - 1:
         catalan_num = 0
         for k in range(m + 1):
             catalan_num += nums[k] * nums[m - k]
