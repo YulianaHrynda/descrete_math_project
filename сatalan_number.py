c9cce5aa0d8bd5dd25b82e1c23f483745d1ed610
Viktoriia Lushpak <lushpak.pn@ucu.edu.ua>
2023-12-06 12:40:09 +0200

    parenthesis sequences count

--- сatalan_number.py
+++ сatalan_number.py
@@ -4,8 +4,38 @@ def integer_paths():
 def diagonals():
     pass
 
-def parenthesis_sequences():
-    pass
+def parenthesis_sequences(n: int) -> int:
+    """
+    Returns the amount of possible parenthesis sequences
+    of length 2n.
+
+    >>> parenthesis_sequences(3)
+    5
+    >>> parenthesis_sequences(0)
+    0
+    >>> parenthesis_sequences(4)
+    14
+    """
+    if not isinstance(n, int) or n < 0:
+        return "Wrong input: n has to be >= 0 Try again."
+    if n == 0:
+        return n
+
+    output = []
+    parenthesis = ''
+    length = 2*n
+
+    def parenthesis_generator(opening, closing, parenthesis, length, output):
+        if opening + closing == length:
+            output.append(parenthesis)
+        else:
+            if opening < length/2:
+                parenthesis_generator(opening+1, closing, parenthesis + '(', length, output)
+            if closing < opening:
+                parenthesis_generator(opening, closing+1, parenthesis + ')', length, output)
+
+    parenthesis_generator(0, 0, parenthesis, length, output)
+    return len(output)
 
 def catalan_numbers(n: int, m: int = 1, nums: list = [1, 1]):
     """
