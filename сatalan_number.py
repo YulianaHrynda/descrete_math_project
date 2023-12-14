4e93a4d3d51d752f3c9856a4500d93a8b8f22679
YulianaHrynda <146963885+YulianaHrynda@users.noreply.github.com>
2023-12-14 15:36:09 +0200

    Update сatalan_number.py

--- сatalan_number.py
+++ сatalan_number.py
@@ -135,7 +135,7 @@ def catalan_numbers(n: int, m: int = 1, nums: list = [1, 1]):
     """
     The function that finds the first n Catalan numbers using a recursive formula
     >>> catalan_numbers(10)
-    [1, 1, 2, 5, 14, 42, 132, 429, 1430, 4862, 16796]
+    [1, 1, 2, 5, 14, 42, 132, 429, 1430, 4862]
     """
     if n == 0:
         return [1]
